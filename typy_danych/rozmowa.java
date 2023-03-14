aa9ffb0905ecaa21f93e50b78321ee22672df43b
Robert Tarnas <robert.tarnas@protonmail.com>
2023-03-14 17:00:09 +0100

    lekcja druga

---
+++ typy_danych/rozmowa.java
@@ -0,0 +1,14 @@
+package typy_danych;
+
+public class rozmowa {
+    public static void main(String[] args) {
+        String inicjaly = "J.K";
+        int wiek = 25;
+        boolean odp = true;
+
+        System.out.println("Cześć, " + inicjaly + "." + " Słyszałem, że masz " + wiek +" lat i uczysz się programowania, czy to prawda?");
+        System.out.println(odp);
+        System.out.println("Czy twoje inicjaly to: " + inicjaly + " ?");
+        System.out.println("Tak, " + odp);
+    }
+}
